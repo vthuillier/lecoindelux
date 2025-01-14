a6e3c8ceee01a9770fd005cf6c17e2004257cd18
Valentin THUILLIER <valentin.thuillier@luxferre-code.fr>
2025-01-14 18:46:49 +0100

    Ajout de la page d'accueil

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  age: string;
+  job: string;
+  articles: any;
+  
+  constructor() {
+    
+    const myBirthday = new Date('2004-07-30');
+    const today = new Date();
+    const diff = today.getTime() - myBirthday.getTime();
+    const age = Math.floor(diff / (1000 * 60 * 60 * 24 * 365.25));
+    this.age = age.toString();
+    this.job = 'alternant développeur chez Extern SN';
+  }
 
 }
