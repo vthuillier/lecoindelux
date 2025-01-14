2359cc812799cb8edebe081b1c090f044b7c1f9d
Valentin THUILLIER <valentin.thuillier@luxferre-code.fr>
2025-01-14 18:21:25 +0100

    Header et Footer ajoutés au dépôt

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from './home/home.component';
 
-export const routes: Routes = [];
+const websiteTitle = 'Le coin de Lux';
+
+export const routes: Routes = [
+
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent, title: `${websiteTitle} - Menu principal`},
+
+];
